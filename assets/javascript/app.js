0ced6fd5b22fe8fd2575d44ca8f78956f43553a2
Rochelle Schofield <CodeGirl@macusers-MacBook-Pro.local>
2018-07-12 17:19:08 -0400

    new api

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -228,3 +228,55 @@ $(document).ready( function() {
     $("#quote").hide();
     document.getElementById('myDayTrigger').style.display = "none";
 }); // closing ready function
+
+
+// news API functionality
+$(document).ready(function(){
+    $('.modal').modal();
+    });
+
+    var newsAPI = '2cbc0b4812554558a06c7e9c28d05b49';
+    var newsQueryUrl = 'https://newsapi.org/v2/top-headlines?country=us&apiKey=';
+    //var response = 'newsQueryUrl' + 'newsAPI';
+
+    $.ajax ({
+        url: newsQueryUrl + newsAPI, 
+        method: "GET"
+    }).then(function(res) {
+
+        console.log(res);
+
+        for (var i = 0; i < res.articles.length; i++) {
+
+            //console.log (res.articles[i].description);
+
+            var item = $('<li>');
+            //item.text(res.articles[i].title);
+            var newsHeader = $('<div id="newsHeader">');
+            newsHeader.text(res.articles[i].title);
+            var newsImage = $('<img class="materialboxed" width="450">');
+            newsImage.attr("src",res.articles[i].urlToImage);
+            
+            
+           //var newsButton = $('a href="newsButton"<button>Visit page now</button>')
+            var newsButton = $('<button <a> Visit Page Now>').attr("href",res.articles[i].url);
+           // newsButton.click("href",res.articles[i].url);
+            //newsButton.attr("href",res.articles[i].url);
+            //var newsUrl = $('<a>').attr("href",res.articles[i].url);
+            //newsUrl.attr("href",res.articles[i].url);
+            //console.log (res.articles[i].url);
+            //console.log ('I am your button');
+            
+
+            //<a href="#"><button>Text</button></a>
+            // $('.newsButton').click(function() {
+            //window.location = (res.articles[i].url) + this.url;
+            //});
+            item.append(newsHeader);
+            item.append(newsImage);
+            //item.append(newsUrl);
+            item.append(newsButton)
+
+            $('#breakingNews').append(item);
+        } 
+    }); 
